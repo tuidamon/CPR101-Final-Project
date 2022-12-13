df45f84323044aef50c8bd32db339743f22a56b9
tuidamon <60093590+tuidamon@users.noreply.github.com>
2022-12-12 23:06:42 -0500

    Revert "Version_2"

--- converting.c
+++ converting.c
@@ -21,20 +21,5 @@ void converting() { // does not return value
 
 
 /* Version 2 */
-	printf("*** Start of Converting Strings to double Demo ***\n");  //print out the start of the Demo
-	char doubleString[BUFFER_SIZE]; //create an array of char called doubleString with size of BUFFER_SIZE
-	double doubleNumber;  //create a double called doubleNumber
-	do
-	{
-		printf("Type the doduble numeric string (q - to quit):\n"); //define the value if it is not q,record it 
-		fgets(doubleString, BUFFER_SIZE, stdin);  //read user's input and store it into doubleString
-		doubleString[strlen(doubleString) - 1] = '\0'; //set the last character of the string to '\0' (Terminator), prevent buffer flow
-		if (strcmp(doubleString, "q") != 0)  // if the string is not empty
-		{
-			doubleNumber = atof(doubleString); //converts a string into an integer number
-			printf("Converted number is %f\n", doubleNumber); //print out the output
-		}
-	} while (strcmp(doubleString, "q") != 0); // compare the string with q if the string is equal to q stop loopinf 
-	printf("*** End of Converting Strings to double Demo ***\n\n"); //print out the end of the Demo
 
 }
--- manipulating.c
+++ manipulating.c
@@ -25,29 +25,6 @@ void manipulating() {
     printf("*** End of Concatenating Strings Demo ***\n\n");
 
 /* Version 2 */
-    printf("*** Start of Comparing Strings Demo ***\n");
-    char compare1[BUFFER_SIZE];   //create an array of char called compare1 with size of BUFFER_SIZE
-    char compare2[BUFFER_SIZE];   //create an array of char called compare2 with size of BUFFER_SIZE
-    int result;             //create an int called result 
-    do
-    {
-        printf("Type the 1st string to compare (q - to quit):\n");
-        fgets(compare1, BUFFER_SIZE, stdin);    //read user's input and store it into compare1
-        compare1[strlen(compare1) - 1] = '\0';   //set the last character of the string to '\0' (Terminator), prevent buffer flow
-        if ((strcmp(compare1, "q") != 0));     //compare the string with "q"
-        {
-            printf("Type the 2nd string to compare:\n");
-            fgets(compare2, BUFFER_SIZE, stdin);     //read user's input and store it into compare2
-            compare2[strlen(compare2) - 1] = '\0';   //set the last character of the string to '\0' (Terminator), prevent buffer flow
-            result = strcmp(compare1, compare2);     //compare the value of two string
-            if (result < 0)                         //when the value is different and compare1 is less than compare2
-                printf("\'%s\' string is less than \'%s\'\n", compare1, compare2);  //print out the message
-            else if(result == 0)                     //when the value is the same
-                printf("\'%s\' string is equal to \'%s\'\n", compare1, compare2);
-            else                                     //when the value is different and compare1 is greater than compare2
-                printf("\'%s\' string is greater than \'%s\'\n", compare1, compare2);
-            
-        }
-    } while (strcmp(compare1, "q") != 0);      // the loop wont quit unless you enter a q input.
-    printf("*** End of Concatenating Strings Demo ***\n\n");
+
+
 }
